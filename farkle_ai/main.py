5144c3064dad6d1152c11bd0d5a916d062abbb56
kucerj64 <kucerj64@cvut.cz>
2025-05-08 11:43:57 +0200

    feat: Simple main.py for testing GUI

---
+++ farkle_ai/main.py
@@ -0,0 +1,22 @@
+"""Starts the farkle game loop and loads GUI."""
+
+import tkinter as tk
+from game.state import GameState, Parameters
+from game.engine import FarkleEngine
+from game.gui import FarkleGUI
+
+if __name__ == "__main__":
+    # --- Game Setup ---
+    # Could prompt for players/score here later if desired
+    params = Parameters(player_count=2, score_to_win=5000)
+    initial_state = GameState(parameters=params)
+    engine = FarkleEngine()
+
+    # --- GUI Setup ---
+    root = tk.Tk()
+    gui = FarkleGUI(root, initial_state, engine)
+
+    # --- Start GUI Main Loop ---
+    root.mainloop()
+
+    print("Game Over.")
